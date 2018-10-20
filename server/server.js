7098bbe7aff64c45544b960d0e15fe7a91127790
Leon <prijatelji.su.u.hrvatskoj@gmail.com>
2018-10-20 18:19:34 +0100

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,11 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    // socket.emit('newMessage', {
-    //     from: 'Mike',
-    //     text: "whatsup",
-    //     createdAt: 123
-    // });
+    socket.emit('newMessage', {
+        from: 'ADMIN',
+        text: 'Welcome to the chat',
+        createdAt: new Date().getTime()   
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'ADMIN',
+        text: 'New user just joined chat',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('create message', message);
@@ -28,7 +34,13 @@ io.on('connection', (socket) => {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
