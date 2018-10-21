92d3fb50486245d52791e9f086012bd07b4076ad
Leon <prijatelji.su.u.hrvatskoj@gmail.com>
2018-10-21 21:05:07 +0100

    added autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage =  messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    };
+};
+
 socket.on('connect', function () {
     console.log('connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formatedTime}: ${message.text}`);
@@ -35,6 +52,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formatedTime}: `).append(`<a href="${message.url}" target="_blank">Location</a>`);
