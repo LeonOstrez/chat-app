bd9d7f32e75a6cfb1b27510bf95b847a750c0fe7
Leon <prijatelji.su.u.hrvatskoj@gmail.com>
2018-10-22 19:20:37 +0100

    send messages only to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -84,7 +84,6 @@ jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     var messageTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
-        from: "User",
         text: messageTextBox.val()
     }, function () {
         messageTextBox.val('');
